0cb91d0454a25f772c8fd9c3ae965c5b3962d19e
Naveed ul Islam <naveed@alislam.org>
2018-06-09 14:53:21 -0400

    Orbiter -> Arbiter in 4:36 EN Plus UrduSG -> UrduTS

--- export_es.sh
+++ export_es.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -XDELETE "http://localhost:9200/hq"
 curl -XPUT "http://localhost:9200/hq" -d @HQ_Artifacts_Prod/mapping.json
-curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQv7.5.txt; clear
+curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQ.json; clear
 curl -XGET localhost:9200/hq/_stats
