ff654070c9f39e37edd9badf0c3b8708d76d3e75
Naveed ul Islam <naveed@alislam.org>
2018-06-16 12:48:11 -0400

    .

--- export_es.sh
+++ export_es.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -XDELETE "http://localhost:9200/hq"
-curl -XPUT "http://localhost:9200/hq" -d @HQ_Artifacts_Prod/mapping.json
+curl -XPUT "http://localhost:9200/hq" -d @mapping.json
 curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQ.json; clear
 #curl -XGET localhost:9200/hq/_stats
 sleep 7
