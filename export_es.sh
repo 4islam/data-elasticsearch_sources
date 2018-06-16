7a59bbece23f471c41225270b363035dd72554cc
Naveed ul Islam <naveed@alislam.org>
2018-06-16 12:44:04 -0400

    Jenkins pipeline testing

--- export_es.sh
+++ export_es.sh
@@ -2,4 +2,5 @@
 curl -XDELETE "http://localhost:9200/hq"
 curl -XPUT "http://localhost:9200/hq" -d @HQ_Artifacts_Prod/mapping.json
 curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQ.json; clear
-curl -XGET localhost:9200/hq/_stats
+#curl -XGET localhost:9200/hq/_stats
+curl -s -XGET localhost:9200/hq/_stats/docs | egrep -o "\"count\":\d+" | head -n1
