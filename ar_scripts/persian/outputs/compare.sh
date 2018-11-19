ad47190711314a2ea512997e605af3cb67cc836f
Naveed ul Islam <naveed@alislam.org>
2018-11-19 14:29:14 -0500

    spacing added

--- ar_scripts/persian/outputs/compare.sh
+++ ar_scripts/persian/outputs/compare.sh
@@ -5,6 +5,6 @@ echo '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style>div{white-space: pre;} *{font-family: 'Times New Roman'; font-size: 38px; line-height:1.7em; text-align: justify;}</style></head>
 <body dir="rtl"><div>' > $output
 #locale >> $output
-dwdiff -s -d" " -w $'<span style="color:blue">' -x $'</span>' -y $'<span  style="color:red">' -z $'</span>' ar_scripts/persian/original/tokenized_100.txt ar_scripts/persian/outputs/verses_100.txt >> $output
+dwdiff -s -d" " -w $' <span style="color:blue">' -x $'</span> ' -y $' <span  style="color:red">' -z $'</span> ' ar_scripts/persian/original/tokenized_100.txt ar_scripts/persian/outputs/verses_100.txt >> $output
 echo "</div></body></html>" >> $output
 open -a safari $output
