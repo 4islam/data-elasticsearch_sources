caf80e55fe815e03b69ea025e3b287eb3cfe4cdb
Naveed ul Islam <naveed@alislam.org>
2018-12-03 13:21:37 -0500

    Now added input from command line argument

--- ar_scripts/persian/outputs/compare.sh
+++ ar_scripts/persian/outputs/compare.sh
@@ -7,8 +7,8 @@ echo '<html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style>div{white-space: pre;} *{font-family: 'noorehuda'; font-size: 38px; line-height:1.7em; text-align: justify;}</style></head>
 <body dir="rtl"><div>' > $output
 #locale >> $output
-dwdiff -s -d" " -w $' <span style="color:blue">' -x $'</span> ' -y $' <span  style="color:red">' -z $'</span> ' ar_scripts/persian/original/tokenized_100.txt ar_scripts/persian/outputs/verses_100.txt >> $output
+dwdiff -s -d" " -w $' <span style="color:blue">' -x $'</span> ' -y $' <span  style="color:red">' -z $'</span> ' ar_scripts/persian/original/tokenized_$1.txt ar_scripts/persian/outputs/verses_$1.txt >> $output
 echo "</div></body></html>" >> $output
 #open -a "safari" $output
 open -a "Google Chrome" $output
-dwdiff -s -d" " ar_scripts/persian/original/tokenized_100.txt ar_scripts/persian/outputs/verses_100.txt 2> $outputstats
+dwdiff -s -d" " ar_scripts/persian/original/tokenized_$1.txt ar_scripts/persian/outputs/verses_$1.txt 2> $outputstats
