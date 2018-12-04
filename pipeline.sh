1b9b8fd4c49e7eb42f5000cf711979c1e44b59ae
Naveed ul Islam <naveed@alislam.org>
2018-12-04 13:58:46 -0500

    Automation updated to compare all batched portions of entire Holy Qur'an

---
+++ pipeline.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+s=1;    if [ -n "$1" ]; then s=$1; fi
+l=100;  if [ -n "$2" ]; then l=$2; fi
+b=1;    if [ -n "$3" ]; then b=$3; fi
+
+curl --output /dev/null -XDELETE http://localhost:9200/hq; sleep 1;
+curl -XPUT localhost:9200/hq/ --data-binary @mapping.json;
+for i in `seq 1 $b`; do
+  #echo Tail of ... $((2*$l)), starting from $((2*$i*$l))
+  cat hQ.json | head -n$((2*$i*$l)) | tail -n$((2*$l)) | \
+    curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @- --output /dev/null; sleep 5;
+  curl -s -XGET localhost:9200/hq/_stats/docs;
+  node ar_scripts/persian/quran_ar_persian.js $((($i-1)*$l+$s)) $l > ar_scripts/persian/outputs/verses_$(($i*$l)).txt;
+
+  cat ar_scripts/persian/original/tokenized.txt | head -n$(($i*$l)) | tail -n$(($l)) \
+        > ar_scripts/persian/original/tokenized_$(($i*$l)).txt
+
+  ./ar_scripts/persian/outputs/compare.sh $(($i*$l))
+done
