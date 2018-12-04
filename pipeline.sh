4b3dd4f0b40c8935eb1605cebe67dcf343340fc6
Naveed ul Islam <naveed@alislam.org>
2018-12-04 16:48:37 -0500

    Pipeline updated to get starting batch option working

--- pipeline.sh
+++ pipeline.sh
@@ -7,13 +7,13 @@ curl --output /dev/null -XDELETE http://localhost:9200/hq; sleep 1;
 curl -XPUT localhost:9200/hq/ --data-binary @mapping.json;
 for i in `seq 1 $b`; do
   #echo Tail of ... $((2*$l)), starting from $((2*$i*$l))
-  cat hQ.json | head -n$((2*$i*$l)) | tail -n$((2*$l)) | \
+  cat hQ.json | head -n$((2*$i*$l+$s*2-2)) | tail -n$((2*$l)) | \
     curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @- --output /dev/null; sleep 5;
   curl -s -XGET localhost:9200/hq/_stats/docs;
-  node ar_scripts/persian/quran_ar_persian.js $((($i-1)*$l+$s)) $l > ar_scripts/persian/outputs/verses_$(($i*$l)).txt;
+  node ar_scripts/persian/quran_ar_persian.js $((($i-1)*$l+$s)) $l > ar_scripts/persian/outputs/verses_$(($i*$l+$s-1)).txt;
 
-  cat ar_scripts/persian/original/tokenized.txt | head -n$(($i*$l)) | tail -n$(($l)) \
-        > ar_scripts/persian/original/tokenized_$(($i*$l)).txt
+  cat ar_scripts/persian/original/tokenized.txt | head -n$(($i*$l+$s-1)) | tail -n$(($l)) \
+        > ar_scripts/persian/original/tokenized_$(($i*$l+$s-1)).txt
 
-  ./ar_scripts/persian/outputs/compare.sh $(($i*$l))
+  ./ar_scripts/persian/outputs/compare.sh $(($i*$l+$s-1))
 done
