d269b9656358115b227fdd4003dafccb382953fe
Naveed ul Islam <naveed@alislam.org>
2018-12-04 21:03:39 -0500

    Sleep increased

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,7 @@ s=1;    if [ -n "$1" ]; then s=$1; fi
 l=100;  if [ -n "$2" ]; then l=$2; fi
 b=1;    if [ -n "$3" ]; then b=$3; fi
 
-curl --output /dev/null -XDELETE http://localhost:9200/hq; sleep 1;
+curl --output /dev/null -XDELETE http://localhost:9200/hq; sleep 3;
 curl -XPUT localhost:9200/hq/ --data-binary @mapping.json;
 for i in `seq 1 $b`; do
   #echo Tail of ... $((2*$l)), starting from $((2*$i*$l))
