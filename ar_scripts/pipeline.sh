b49b9206da4533b9621c09c58893e0dfe8af0435
Naveed ul Islam <naveed@alislam.org>
2018-12-04 21:48:45 -0500

    pipeline update

--- ar_scripts/pipeline.sh
+++ ar_scripts/pipeline.sh
@@ -2,6 +2,7 @@
 s=1;    if [ -n "$1" ]; then s=$1; fi
 l=100;  if [ -n "$2" ]; then l=$2; fi
 b=1;    if [ -n "$3" ]; then b=$3; fi
+o=chrome; if [ -n "$4" ]; then o=$4; fi
 
 curl --output /dev/null -XDELETE http://localhost:9200/hq; sleep 3;
 curl -XPUT localhost:9200/hq/ --data-binary @../mapping.json;
@@ -15,5 +16,5 @@ for i in `seq 1 $b`; do
   cat ./persian/original/tokenized.txt | head -n$(($i*$l+$s-1)) | tail -n$(($l)) \
         > ./persian/original/tokenized_$(($i*$l+$s-1)).txt
 
-  ./persian/outputs/compare.sh $(($i*$l+$s-1)) $4
+  ./persian/outputs/compare.sh $(($i*$l+$s-1)) $o
 done
