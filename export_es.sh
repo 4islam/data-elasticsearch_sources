63c287b61e6ce53a36c805e81b7ef9ff463eff06
Naveed ul Islam <naveed@alislam.org>
2020-04-06 15:36:50 -0400

    Updated for this repo

--- export_es.sh
+++ export_es.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -s -XDELETE "http://localhost:9200/hq"
-curl -s -XPUT "http://localhost:9200/hq" -d @~/uploads/mapping.json
-curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @~/uploads/hQ.json; clear
+curl -s -XPUT "http://localhost:9200/hq" -d @mapping.json
+curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQ.json; clear
 #curl -XGET localhost:9200/hq/_stats
 sleep 7
 curl -s -XGET localhost:9200/hq/_stats/docs | egrep -o "\"count\":[0-9]+" | head -n1
