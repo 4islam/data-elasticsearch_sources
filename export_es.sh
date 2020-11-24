6572ffdcefc9e488ba04178bfd0036f49e0733e4
Naveed ul Islam <naveed@alislam.org>
2020-11-24 17:39:26 -0500

    Headers updated.

--- export_es.sh
+++ export_es.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-curl -s -XDELETE "http://localhost:9200/hq"
-curl -s -XPUT "http://localhost:9200/hq" -d @mapping.json
-curl -s -XPOST localhost:9200/hq/verse/_bulk --data-binary @hQ.json; clear
+curl -s -XDELETE "http://localhost:9200/hq" -H 'Content-Type: application/json'
+curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json
+curl -s -XPOST localhost:9200/hq/verse/_bulk curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json --data-binary @hQ.json; clear
 #curl -XGET localhost:9200/hq/_stats
 sleep 7
-curl -s -XGET localhost:9200/hq/_stats/docs | egrep -o "\"count\":[0-9]+" | head -n1
+curl -s -XGET localhost:9200/hq/_stats/docs curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json | egrep -o "\"count\":[0-9]+" | head -n1
