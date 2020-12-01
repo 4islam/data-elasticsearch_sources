6a4887817d018cb64f5c6bf4b867da7ce583698c
Naveed ul Islam <naveed@alislam.org>
2020-12-01 13:28:03 -0500

    Minor updates.

--- export_es.sh
+++ export_es.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 curl -s -XDELETE "http://localhost:9200/hq" -H 'Content-Type: application/json'
+clear;
 curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json
 curl -s -XPOST localhost:9200/hq/verse/_bulk curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json --data-binary @hQ.json; clear
 #curl -XGET localhost:9200/hq/_stats
