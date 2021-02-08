ff5d7183766fbd42f957722cad5a2cfda1fe832f
Naveed ul Islam <naveed@alislam.org>
2021-02-08 18:14:08 -0500

    Updated for ES 7.x

--- export_es.sh
+++ export_es.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-curl -s -XDELETE "http://localhost:9200/hq" -H 'Content-Type: application/json'
-clear;
-curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json
-curl -s -XPOST localhost:9200/hq/verse/_bulk curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json --data-binary @hQ.json; clear
+curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+echo "\n"
+curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
+echo "\n"
+curl -s -XDELETE http://localhost:9200/hq -H 'Content-Type: application/json'
+echo "\n"
+curl -s -XPUT http://localhost:9200/hq -d @mapping.json -H 'Content-Type: application/json'
+echo "\n"
+curl -s -XPOST localhost:9200/hq/_bulk --data-binary @hQ.json -H 'Content-Type: application/json'; clear
+echo "\n"
 #curl -XGET localhost:9200/hq/_stats
-sleep 7
-curl -s -XGET localhost:9200/hq/_stats/docs curl -s -XPUT "http://localhost:9200/hq" -H 'Content-Type: application/json' -d @mapping.json | egrep -o "\"count\":[0-9]+" | head -n1
+sleep 10
+curl -s -XGET localhost:9200/hq/_stats/docs -H 'Content-Type: application/json' | egrep -o "\"count\":[0-9]+" | head -n1
