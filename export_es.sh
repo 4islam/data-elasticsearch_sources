296254869d017d71ffbac57dd34db48bbaede56d
Naveed ul Islam <naveed@alislam.org>
2021-02-23 10:22:35 -0500

    Port added as potential argument

--- export_es.sh
+++ export_es.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
-echo "\n"
-curl -XPUT -H "Content-Type: application/json" http://localhost:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
-echo "\n"
-curl -s -XDELETE http://localhost:9200/hq -H 'Content-Type: application/json'
-echo "\n"
-curl -s -XPUT http://localhost:9200/hq -d @mapping.json -H 'Content-Type: application/json'
-echo "\n"
-curl -XPUT "localhost:9200/hq/_settings?pretty" -H 'Content-Type: application/json' -d'{ "index.requests.cache.enable": true }'
-echo "\n"
-curl -s -XPOST localhost:9200/hq/_bulk --data-binary @hQ.json -H 'Content-Type: application/json'; clear
-echo "\n"
+p=9200;    if [ -n "$1" ]; then p=$1; fi
+
+curl -XPUT -H "Content-Type: application/json" http://localhost:$p/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+echo ""
+curl -XPUT -H "Content-Type: application/json" http://localhost:$p/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
+echo ""
+curl -s -XDELETE "http://localhost:$p/hq" -H 'Content-Type: application/json'
+echo ""
+curl -s -XPUT "http://localhost:$p/hq" -d @mapping.json -H 'Content-Type: application/json'
+echo ""
+curl -XPUT "localhost:$p/hq/_settings?pretty" -H 'Content-Type: application/json' -d'{ "index.requests.cache.enable": true }'
+echo ""
+curl -s -XPOST "localhost:$p/hq/_bulk" --data-binary @hQ.json -H 'Content-Type: application/json'; clear
+echo ""
 #curl -XGET localhost:9200/hq/_stats
 sleep 10
-curl -s -XGET localhost:9200/hq/_stats/docs -H 'Content-Type: application/json' | egrep -o "\"count\":[0-9]+" | head -n1
+curl -s -XGET "localhost:$p/hq/_stats/docs" -H 'Content-Type: application/json' | egrep -o "\"count\":[0-9]+" | head -n1
